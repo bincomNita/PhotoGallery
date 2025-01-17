cc988ec24ef15c2ecf65c3ce4fe7b587dc0093c6
bincomNita <nitakalukhe.bincom@gmail.com>
2025-01-17 19:23:06 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhotoGallery.Models;
+namespace PhotoGallery.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) :DbContext(options)
+    {
+        public DbSet<GalleryItem> GalleryItems { get; set; }
+    }
+}
